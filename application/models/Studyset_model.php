42c839963b9003c308f7731507f09405c4bfc9c8
nehashukla0310 <nehashukla0310@gmail.com>
2020-10-15 12:57:15 +0530

    studyset issue query test

--- application/models/Studyset_model.php
+++ application/models/Studyset_model.php
@@ -68,7 +68,7 @@ class Studyset_model extends CI_Model {
         $this->db->where('s.status',1);
         $this->db->limit(PER_PAGE, $page * PER_PAGE);
         $study_sets = $this->db->get()->result_array(); 
-        echo $this->db->last_query();die;
+        // echo $this->db->last_query();die;
         // echo $this->db->last_query();die;   
         $final_study_set = array();
         foreach ($study_sets as $key => $value) {
