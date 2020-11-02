7abefb76cf1268773f3334e92c55d6ec6885dea6
nehashukla0310 <nehashukla0310@gmail.com>
2020-11-02 20:27:01 +0530

    studyset unshare functionality

--- application/views/studyset/study-sets.php
+++ application/views/studyset/study-sets.php
@@ -570,7 +570,7 @@ foreach ($getdata as $key => $value) {
 			data : {"study_set_id" : share_studyset, 'peer_id': peer_id},
 			success:function(result) {
 				$('#share_count_'+share_studyset).html(result);
-				$("#action_"+peer_id).html('<button type="button" class="like" onclick="unshareToPeer('+peer_id+')>shared</button>');
+				$("#action_"+peer_id).html('<button type="button" class="like" onclick="unshareToPeer('+peer_id+')">shared</button>');
 				// $("#share_studyset").val('');
 			}
 		})
