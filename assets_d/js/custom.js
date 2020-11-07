7f1715da77422a958b174122a7e39ae2698f3a87
nehashukla0310 <nehashukla0310@gmail.com>
2020-11-07 18:57:01 +0530

    custom js update

--- assets_d/js/custom.js
+++ assets_d/js/custom.js
@@ -11,6 +11,9 @@ if($(window).width() <= 479) {
     $('.removeSearch').css('display','none');
 })
 }
+$("#featured_image-document").change(function() {
+    readURLDocument(this);
+});
 });
 
 function wrapupToggle(){
@@ -191,7 +194,237 @@ $('.showResult').on('click',()=>{
 $('.flashcard-controls').removeClass('active');
 $('.flashcards').removeClass('active');
 })
-$( ".match-item" ).draggable();
+$( ".match-item" ).draggable({
+    start: function(e, ui) {
+        $(this).css({"z-index": "5"});
+        drag_id = $(this).attr('id');
+    }
+});
+
+$(".match-item").droppable({
+    drop: function( event, ui ) {
+        var drop_data_id = $(this).attr('data-id');
+        var drag_data_id = $('#'+drag_id).attr('data-id');
+
+        var correct_count = parseInt($('#correct_count').html());
+        var missed_count  = parseInt($('#missed_count').html());
+        var missed_terms  = $('#missed_terms').val();
+
+        var drop_type = $(this).attr("data-type");
+        var drag_type = $('#'+drag_id).attr("data-type");
+
+        if(drop_data_id == drag_data_id){
+            $(this).hide(500);
+            $('#'+drag_id).hide(500);
+
+            if( (missed_terms.split(',').indexOf(drag_data_id) > -1) || (missed_terms.split(',').indexOf(drop_data_id) > -1) ) {
+
+            } else {
+                var n = correct_count+1;
+                var formattedNumber = ("0" + n).slice(-2);
+                $('#correct_count').html(formattedNumber);
+            }
+
+
+            var count = $(".match-item:visible").length;
+            if(count == 2){
+                submitMatchData();
+            }
+        } else {
+            $(this).css({"border-color": "red"});
+            var drop_id = $(this).attr('id');
+
+
+            $('#'+drag_id).css({"border-color": "red"});
+
+            if(drag_type == "term") {
+
+                if(missed_terms != ""){
+                    if( (missed_terms.split(',').indexOf(drag_data_id) != 0)){
+                        $('#missed_terms').val(missed_terms+','+drag_data_id);
+                        var n = missed_count+1;
+                        var formattedNumber = ("0" + n).slice(-2);
+                        $('#missed_count').html(formattedNumber);
+                    }
+                } else {
+                    $('#missed_terms').val(drag_data_id);
+                    var n = missed_count+1;
+                    var formattedNumber = ("0" + n).slice(-2);
+                    $('#missed_count').html(formattedNumber);
+                }
+
+
+            } else if(drop_type == "term") {
+
+                if(missed_terms != ""){
+                    if( (missed_terms.split(',').indexOf(drop_data_id) != 0)){
+                        $('#missed_terms').val(missed_terms+','+drop_data_id);
+                        var n = missed_count+1;
+                        var formattedNumber = ("0" + n).slice(-2);
+                        $('#missed_count').html(formattedNumber);
+                    }
+                } else {
+                    $('#missed_terms').val(drop_data_id);
+                    var n = missed_count+1;
+                    var formattedNumber = ("0" + n).slice(-2);
+                    $('#missed_count').html(formattedNumber);
+                }
+
+
+            }
+
+            var rand_x = 0;
+            var rand_y = 0;
+            var i = 0;
+            var smallest_overlap = 9007199254740992;
+            var best_choice;
+            var area;
+            for (i = 0; i < maxSearchIterations; i++) {
+                rand_x = Math.round(min_x + ((max_x - min_x) * (Math.random() % 1)));
+                rand_y = Math.round(min_y + ((max_y - min_y) * (Math.random() % 1)));
+                area = {
+                    x: rand_x,
+                    y: rand_y,
+                    width: $(this).width(),
+                    height: $(this).height()
+                };
+                var overlap = calc_overlap(area);
+                if (overlap < smallest_overlap) {
+                    smallest_overlap = overlap;
+                    best_choice = area;
+                }
+                if (overlap === 0) {
+                    break;
+                }
+            }
+
+            filled_areas.push(best_choice);
+            $('#'+drag_id).animate({
+                left: rand_x,
+                top: rand_y
+            });
+            setTimeout(function(){
+                    $('#'+drop_id).stop().animate({"border-color": "#eee"}, "slow");
+                    $('#'+drag_id).stop().animate({"border-color": "#eee"}, "slow");
+
+                },
+                500);
+
+        }
+
+
+
+    }
+});
+
+
+
+$('#nextFilp').click(function() {
+    if (!$('.choose_btn').hasClass('selected') && !$('.current').hasClass('last')) {
+        var markCount = parseInt($('#markCount').html());
+        var n = markCount+1;
+        var formattedNumber = ("0" + n).slice(-2);
+        $('#markCount').html(formattedNumber);
+
+        var remainingCount = parseInt($('#remainingCount').html());
+        var n = remainingCount-1;
+        var formattedNumber = ("0" + n).slice(-2);
+        $('#remainingCount').html(formattedNumber);
+    }
+    $('.choose_btn').removeClass('selected');
+    $('.current').removeClass('current').hide()
+        .next().show().addClass('current');
+    if ($('.current').hasClass('last')) {
+        $('#nextFilp').addClass('disabled');
+        $("#nextFilp").css("pointer-events", "none");
+
+    }
+    if($('.current').hasClass("is-flipped")){
+        $('.flashcard-controls').addClass('active');
+    } else {
+        $('.flashcard-controls').removeClass('active');
+    }
+
+    var current = $('.current').attr("data-id");
+    var termsSeen = $('#termsSeen').val();
+
+    if(termsSeen != ''){
+        if( (termsSeen.split(',').indexOf(current) > -1) ) {
+            var correctTerms = $('#correctTerms').val();
+            var incorrectTerms = $('#incorrectTerms').val();
+            var notSureTerms = $('#notSureTerms').val();
+            var hideTerms = $('#hideTerms').val();
+            if(correctTerms != ''){
+                if( (correctTerms.split(',').indexOf(current) > -1)) {
+                    $('.correct').addClass('selected');
+                }
+            } if(incorrectTerms != ''){
+                if( (incorrectTerms.split(',').indexOf(current) > -1)) {
+                    $('.incorrect').addClass('selected');
+                }
+            } if(notSureTerms != ''){
+                if( (notSureTerms.split(',').indexOf(current) > -1)) {
+                    $('.not-sure').addClass('selected');
+                }
+            } if(hideTerms != ''){
+                if( (hideTerms.split(',').indexOf(current) > -1)) {
+                    $('.hidecard').addClass('selected');
+                }
+            }
+        }
+    }
+
+    $('#prevFlip').removeClass('disabled');
+    $("#prevFlip").css("pointer-events", "auto");
+});
+
+$('#prevFlip').click(function() {
+    $('.choose_btn').removeClass('selected');
+    $('.current').removeClass('current').hide()
+        .prev().show().addClass('current');
+    if ($('.current').hasClass('first')) {
+        $('#prevFlip').addClass('disabled');
+        $("#prevFlip").css("pointer-events", "none");
+    }
+    if($('.current').hasClass("is-flipped")){
+        $('.flashcard-controls').addClass('active');
+    } else {
+        $('.flashcard-controls').removeClass('active');
+    }
+
+    var current = $('.current').attr("data-id");
+    var termsSeen = $('#termsSeen').val();
+
+    if(termsSeen != ''){
+        if( (termsSeen.split(',').indexOf(current) > -1) ) {
+            var correctTerms = $('#correctTerms').val();
+            var incorrectTerms = $('#incorrectTerms').val();
+            var notSureTerms = $('#notSureTerms').val();
+            var hideTerms = $('#hideTerms').val();
+            if(correctTerms != ''){
+                if( (correctTerms.split(',').indexOf(current) > -1)) {
+                    $('.correct').addClass('selected');
+                }
+            } if(incorrectTerms != ''){
+                if( (incorrectTerms.split(',').indexOf(current) > -1)) {
+                    $('.incorrect').addClass('selected');
+                }
+            } if(notSureTerms != ''){
+                if( (notSureTerms.split(',').indexOf(current) > -1)) {
+                    $('.not-sure').addClass('selected');
+                }
+            } if(hideTerms != ''){
+                if( (hideTerms.split(',').indexOf(current) > -1)) {
+                    $('.hidecard').addClass('selected');
+                }
+            }
+        }
+    }
+
+    $('#nextFilp').removeClass('disabled');
+    $("#nextFilp").css("pointer-events", "auto");
+});
+
 
 $(document).ready(function(){
     $('.likecount a').click(function(){
@@ -199,11 +432,15 @@ $(document).ready(function(){
         $(this).toggleClass('active');
     })
 })
+
+
 //Add Course
 $('.add_course').on('click', function(){
-    var coursebox = $('.courseBox'),
-        cloneBox = $('.courseBox').eq(0).clone(true, true),
-        num = $('.courseBox').length;
+    var coursebox = $('#courseModal .courseBox'),
+        cloneBox = $('#courseModal .courseBox').eq(0).clone(true, true),
+        num = $('#courseModal .courseBox').length;
+    cloneBox.find('input:text').val('');
+    cloneBox.find('.removeCourseBox').show();
     this.disabled = num+1 >=6;
     if(num>=2) {
         $('#courseModal .modal-body').addClass('scroll');
@@ -227,11 +464,65 @@ $(document).on('click','.courseBox > .removeCourseBox', function(){
     if(coursebox.length < 6) {
         $('.add_course').removeClass("disabled");
     }
-})
+});
 
 
 
 
+function validateCourse(){
+
+    var flag1 = 1; var flag2 = 1; var flag3 = 1;
+
+    $('#course_form').find('.course_name').each(function(){
+        if($(this).val() == ''){
+            $(this).css('border-color', 'red');
+            flag1 = 0;
+        } else {
+            $(this).css('border-color', '#ccc');
+        }
+    });
+
+    $('#course_form').find('.professor_first_name').each(function(){
+        if($(this).val() == ''){
+            $(this).css('border-color', 'red');
+            flag2 = 0;
+        } else {
+            $(this).css('border-color', '#ccc');
+        }
+    });
+
+    $('#course_form').find('.professor_last_name').each(function(){
+        if($(this).val() == ''){
+            $(this).css('border-color', 'red');
+            flag3 = 0;
+        } else {
+            $(this).css('border-color', '#ccc');
+        }
+    });
+
+
+    if(flag1 == 0 || flag2 == 0 || flag3 == 0){
+        return false;
+    }
+
+}
+
+
+function showAllCourses(url){
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: {'val': 1},
+        dataType: "json",
+        success: function(result) {
+            $('#courseModalAllBody').html(result.html);
+            $('#courseModalAll').modal('show');
+            if(result.num>=2) {
+                $('#courseModalAll .modal-body').addClass('scroll');
+            }
+        }
+    });
+}
 
 
 
