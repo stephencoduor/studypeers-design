636eaf601c59ef3b640b44bf54cec22397f6b133
Shweta <shwetamishra319@gmail.com>
2020-11-10 21:02:06 +0530

    fixed ajax image issue

--- application/views/user/profile/layouts/footer.php
+++ application/views/user/profile/layouts/footer.php
@@ -727,24 +727,25 @@
         $('#search_friend').on("keyup", function(e){
             var search_value = $(this).val();var html = '';
             var friend = selection_type;   // 0 => requests , 1 => friend
-            if(friend == 0){
-                $('#request_container').html('');
-            }else{
-                $('#friend_container').html('');
-            }
+
 
             $.ajax({
                 url : '<?php echo base_url();?>Profile/searchFriends?keyword='+search_value+"&is_friend="+friend,
                 type : 'get',
                 success:function(result) {
-                     var result_json = JSON.parse(result);
-                    console.log(result_json);
+                    var result_json = JSON.parse(result);
+                    if(friend == 0){
+                        $('#request_container').html('');
+                    }else{
+                        $('#friend_container').html('');
+                    }
                      for(var i = 0; i < result_json.length; i++){
+                         getUsersImage(result_json[i].friend_id);
                          if(friend == 0){
                              html += '<div class="card">';
                              html += '<div class="profileSection">';
                              html += '<div class="profileViewToggleWrapper"><figure>';
-                             html += '<img src="'+getUsersImage(result_json[i].friend_id)+'">';
+                             html += '<img id="img_'+result_json[i].friend_id+'" src="">';
                              html += '</figure><div class="changeView"><h5>'+result_json[i].first_name+' '+result_json[i].last_name+'</h5><p>location name</p>';
                              html += '<div class="followers"><span>25 </span> Followers</div></div></div><div class="followOptionsWrapper"><ul><li class="follower">';
                              html += '<a href="javascript:void(0)">Accept</a></li><li class="follower"><a href="javascript:void(0)">Reject</a></li></ul>';
@@ -752,7 +753,7 @@
                          }else{
                              html += '<div class="card">';
                              html += '<div class="profileSection"><div class="profileViewToggleWrapper"><figure>';
-                             html += '<img src="'+getUsersImage(result_json[i].friend_id)+'">';
+                             html += '<img id="img_'+result_json[i].friend_id+'" src="">';
                              html += '</figure><div class="changeView"><h5>'+result_json[i].first_name+' '+result_json[i].last_name+'</h5><p>location name</p>';
                              html += '<div class="followers"><span>25 </span> Followers</div></div></div><div class="followOptionsWrapper"><ul>';
                              html += '<li data-dismiss="modal" data-toggle="modal" href="#blockUser"><a href="javascript:void(0)">Follow</a>';
@@ -895,13 +896,18 @@
         });
     }
 
+    var base_url = '<?php echo base_url(); ?>';
+
     function getUsersImage(user_id){
         $.ajax({
-            url: '<?php echo base_url();?>Profile/getUsersImageViaAjax',
+            url: base_url+'Profile/getUsersImageViaAjax',
             type: 'post',
             data: {"user_id": user_id},
             success: function (result) {
-                return result;
+                console.log(user_id);
+                var res = JSON.parse(result);
+                console.log(res);
+                document.getElementById("img_"+res.id).src = res.image;
             }
         });
     }
