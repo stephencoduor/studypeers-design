c7f058301c791a56116207ec78cbc561ea13e782
Bhuneshwar Chauhan <bhuneshwar.chauhan@appinventiv.com>
2020-11-16 14:27:35 +0530

    About Page HTML

--- design/assets/js/custom.js
+++ design/assets/js/custom.js
@@ -29,6 +29,37 @@ $('.tool-slide').slick({
       }
     ]
 });
+$('.out-team').slick({
+  infinite: false,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  dots: true,
+  autoplay:true,
+  arrow:false,
+  responsive: [
+  {
+    breakpoint: 1100,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2,
+    }
+  },
+  {
+    breakpoint: 767,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+]
+});
 $('.studyTool-slide').slick({
       infinite: false,
       slidesToShow: 4,
