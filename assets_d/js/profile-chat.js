ac40323aad467c6d8cc2eeab549144de47362a4f
Jatin <jay.pandey45@gmail.com>
2021-01-24 22:11:40 +0530

    update

--- assets_d/js/profile-chat.js
+++ assets_d/js/profile-chat.js
@@ -56,15 +56,15 @@ var CHAT_GROUP_ADDITIONS_SINGLE = {
           if (parseInt(additionaInfo.group_id) != 0) {
             console.log("teting group--->");
             $("#curren_group_name_id").val(additionaInfo.receiverName);
-            $("#single_chat_image_preview").attr(
-              "src",
-              additionaInfo.receiverImage
-            );
           } else {
             $("#current_active_user_group_image_single").val(
               additionaInfo.image
             );
             $("#curren_group_name_id").val(additionaInfo.name);
+            $("#single_chat_image_preview").attr(
+              "src",
+              additionaInfo.receiverImage
+            );
           }
 
           $("#curren_group_members").val(JSON.stringify(groupMemberIds));
