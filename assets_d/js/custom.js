435a66f19b0992c0362c5a5065af6dbb7dbe075e
Bhuneshwar Chauhan <bhuneshwar.chauhan@appinventiv.com>
2021-02-05 14:43:14 +0530

    HTML Feedbacks

--- assets_d/js/custom.js
+++ assets_d/js/custom.js
@@ -582,31 +582,3 @@ $(".navbar-brand-icon").click(function(){
     $('.rightsidemsgbar').css("transform","translateX(0px)");;
     $(".mainContent").removeClass("msgActive")
 });
-
-function touchHandler(event) {
-    var touch = event.changedTouches[0];
-
-    var simulatedEvent = document.createEvent("MouseEvent");
-        simulatedEvent.initMouseEvent({
-        touchstart: "mousedown",
-        touchmove: "mousemove",
-        touchend: "mouseup"
-    }[event.type], true, true, window, 1,
-        touch.screenX, touch.screenY,
-        touch.clientX, touch.clientY, false,
-        false, false, false, 0, null);
-
-    touch.target.dispatchEvent(simulatedEvent);
-    event.preventDefault();
-}
-
-function init() {
-    document.addEventListener("touchstart", touchHandler, true);
-    document.addEventListener("touchmove", touchHandler, true);
-    document.addEventListener("touchend", touchHandler, true);
-    document.addEventListener("touchcancel", touchHandler, true);
-}
-
-$(document).ready(function(){
-    init()
-});
