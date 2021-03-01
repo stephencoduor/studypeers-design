68e6b2732ac5cff5bcd96ca9516d8d96f9c78d85
nehashukla0310 <nehashukla0310@gmail.com>
2021-03-01 22:02:04 +0530

    update

--- application/config/config.php
+++ application/config/config.php
@@ -390,7 +390,7 @@ $config['encryption_key'] = '825u8vse8y86vuf7rvgii8';
 */
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
+$config['sess_expiration'] = 0;
 $config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
