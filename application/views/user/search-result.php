75b89c3ea71ea96016a93471446dcb3a6e1734f1
Bhuneshwar Chauhan <bhuneshwar.chauhan@appinventiv.com>
2021-03-11 10:09:18 +0530

    HTML Feedbacks

---
+++ application/views/user/search-result.php
@@ -0,0 +1,167 @@
+<section class="mainContent">
+    <div class="main-home-wrapper">
+        <div class="tabs-wrappper">
+            <div class="tabslisting">
+                <ul class="nav nav-tabs">
+                    <li class="active"><a data-toggle="tab" href="#peers" aria-expanded="true">Peers</a></li>
+                    <li><a data-toggle="tab" href="#posts" aria-expanded="true">Posts</a></li>
+                    <li><a data-toggle="tab" href="#questions" aria-expanded="true">Questions</a></li>
+                    <li><a data-toggle="tab" href="#documents" aria-expanded="true">Documents</a></li>
+                    <li><a data-toggle="tab" href="#studySets" aria-expanded="true">Study Sets</a></li>
+                    <li><a data-toggle="tab" href="#events" aria-expanded="true">Events</a></li>
+                    <li><a data-toggle="tab" href="#articles" aria-expanded="true">Articles</a></li>
+                    <li><a data-toggle="tab" href="#studySessions" aria-expanded="true">Study Sessions</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="tab-content">
+            <div id="peers" class="tab-pane fade in active">
+                <div class="content-card">
+                    <div class="title-wrap">
+                        <h3>Peers</h3>
+                    </div>
+                    <div class="peers-listing">
+                        <div class="peers-row">
+                            <div class="peer-left-info">
+                                <div class="peers-img-wrap">
+                                    <img src="<?php echo base_url(); ?>assets_d/images/user.jpg" alt="Image"/>
+                                </div>
+                                <div class="basic-info">
+                                    <h3>Full Name</h3>
+                                    <ul>
+                                        <li>University name</li>
+                                        <li>Location name</li>
+                                        <li><a href="">25</a> followers</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="peer-right-info">
+                                <ul>
+                                    <li><a href=""><img src="<?php echo base_url(); ?>assets_d/images/chat-red.svg" alt="Image"/></a></li>
+                                    <li><a href="">Following</a></li>
+                                    <li><a href="">Peer</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="peers-row">
+                            <div class="peer-left-info">
+                                <div class="peers-img-wrap">
+                                    <img src="<?php echo base_url(); ?>assets_d/images/user.jpg" alt="Image"/>
+                                </div>
+                                <div class="basic-info">
+                                    <h3>Full Name</h3>
+                                    <ul>
+                                        <li>University name</li>
+                                        <li>Location name</li>
+                                        <li><a href="">25</a> followers</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="peer-right-info">
+                                <ul>
+                                    <li><a href=""><img src="<?php echo base_url(); ?>assets_d/images/chat-red.svg" alt="Image"/></a></li>
+                                    <li><a href="">Following</a></li>
+                                    <li><a href="">Peer</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="peers-row">
+                            <div class="peer-left-info">
+                                <div class="peers-img-wrap">
+                                    <img src="<?php echo base_url(); ?>assets_d/images/user.jpg" alt="Image"/>
+                                </div>
+                                <div class="basic-info">
+                                    <h3>Full Name</h3>
+                                    <ul>
+                                        <li>University name</li>
+                                        <li>Location name</li>
+                                        <li><a href="">25</a> followers</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="peer-right-info">
+                                <ul>
+                                    <li><a href=""><img src="<?php echo base_url(); ?>assets_d/images/chat-red.svg" alt="Image"/></a></li>
+                                    <li><a href="">Following</a></li>
+                                    <li><a href="">Peer</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="peers-row">
+                            <div class="peer-left-info">
+                                <div class="peers-img-wrap">
+                                    <img src="<?php echo base_url(); ?>assets_d/images/user.jpg" alt="Image"/>
+                                </div>
+                                <div class="basic-info">
+                                    <h3>Full Name</h3>
+                                    <ul>
+                                        <li>University name</li>
+                                        <li>Location name</li>
+                                        <li><a href="">25</a> followers</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="peer-right-info">
+                                <ul>
+                                    <li><a href=""><img src="<?php echo base_url(); ?>assets_d/images/chat-red.svg" alt="Image"/></a></li>
+                                    <li><a href="">Following</a></li>
+                                    <li><a href="">Peer</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="peers-row">
+                            <div class="peer-left-info">
+                                <div class="peers-img-wrap">
+                                    <img src="<?php echo base_url(); ?>assets_d/images/user.jpg" alt="Image"/>
+                                </div>
+                                <div class="basic-info">
+                                    <h3>Full Name</h3>
+                                    <ul>
+                                        <li>University name</li>
+                                        <li>Location name</li>
+                                        <li><a href="">25</a> followers</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="peer-right-info">
+                                <ul>
+                                    <li><a href=""><img src="<?php echo base_url(); ?>assets_d/images/chat-red.svg" alt="Image"/></a></li>
+                                    <li><a href="">Following</a></li>
+                                    <li><a href="">Peer</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="posts" class="tab-pane fade">
+                <h3>Menu 1</h3>
+                <p>Some content in menu 1.</p>
+            </div>
+            <div id="questions" class="tab-pane fade">
+                <h3>Menu 2</h3>
+                <p>Some content in menu 2.</p>
+            </div>
+            <div id="documents" class="tab-pane fade">
+                <h3>Menu 2</h3>
+                <p>Some content in menu 2.</p>
+            </div>
+            <div id="studySets" class="tab-pane fade">
+                <h3>Menu 2</h3>
+                <p>Some content in menu 2.</p>
+            </div>
+            <div id="events" class="tab-pane fade">
+                <h3>Menu 2</h3>
+                <p>Some content in menu 2.</p>
+            </div>
+            <div id="articles" class="tab-pane fade">
+                <h3>Menu 2</h3>
+                <p>Some content in menu 2.</p>
+            </div>
+            <div id="studySessions" class="tab-pane fade">
+                <h3>Menu 2</h3>
+                <p>Some content in menu 2.</p>
+            </div>
+        </div>
+    </div>
+</section>
