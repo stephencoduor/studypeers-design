445be801e0c40c77e5a8eb81858c6d269c8c4d8d
Bhuneshwar Chauhan <bhuneshwar.chauhan@appinventiv.com>
2021-04-08 20:44:12 +0530

    HTML Feedbacks

--- application/views/user/include/header.php
+++ application/views/user/include/header.php
@@ -478,7 +478,10 @@ $last_notification = $this->db->get_where('notification_master', array('user_id'
 								</div>
 								<input type="text" id="search-info" autocomplete="off" value="<?php echo (isset($SearchText)) ? $SearchText : ''; ?>">
 								<div class="removeBadge">
-									<span class="removeSearchIcon" style="display:none;"><i class="fa fa-times"></i></span>
+									<span class="removeSearchIcon" style="display:none;"><svg height="512pt" viewBox="0 0 512 512" width="512pt" xmlns="http://www.w3.org/2000/svg">
+                            <path d="m256 0c-141.164062 0-256 114.835938-256 256s114.835938 256 256 256 256-114.835938 256-256-114.835938-256-256-256zm0 0" fill="#2196f3"></path>
+                            <path d="m368 277.332031h-90.667969v90.667969c0 11.777344-9.554687 21.332031-21.332031 21.332031s-21.332031-9.554687-21.332031-21.332031v-90.667969h-90.667969c-11.777344 0-21.332031-9.554687-21.332031-21.332031s9.554687-21.332031 21.332031-21.332031h90.667969v-90.667969c0-11.777344 9.554687-21.332031 21.332031-21.332031s21.332031 9.554687 21.332031 21.332031v90.667969h90.667969c11.777344 0 21.332031 9.554687 21.332031 21.332031s-9.554687 21.332031-21.332031 21.332031zm0 0" fill="#fafafa"></path>
+                        </svg></span>
 								</div>
 								
 								<input type="hidden" id="searchAction" value="<?php echo base_url('account/searchAllDetails'); ?>">
